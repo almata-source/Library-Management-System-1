d1401748b5949512d5c696d519964af2e59424a0
almata-source <almata@my.cspc.edu.ph>
2025-08-31 16:02:43 +0800

    Update librarymanagement.cpp

--- librarymanagement.cpp
+++ librarymanagement.cpp
@@ -5,6 +5,13 @@ using namespace std;
 //---MAIN---
 int main() {
     Library lib;
-    
-}
 
+    while (true) {
+        cout<< "--- Library Management ---";
+        cout<< "1. Register User\n";
+        cout<< "2.Remove User\n";
+        cout<< "3. Display Users\n";
+        cout << "4. Add Book\n";
+        cout << "5. Remove Book\n";
+    }
+}
